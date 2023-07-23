7a779a98ba05642b057467522b53f4fa511239b6
Swarzstein <zorrillagja@gmail.com>
2023-07-23 01:10:08 -0400

    all testings passing

--- spec/integration/groups_index_spec.rb
+++ spec/integration/groups_index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 # rspec for features
-RSpec.describe 'groups#index', type: :system do
+RSpec.describe 'groups#index', type: :feature do
   let!(:user) { User.create(name: 'Akai', email: "akaiisred@gmail.com", password: "123123") }
   let!(:group) { Group.create(name: 'Medicine', user_id: user.id) }
   let!(:expense) { Expense.create(name: 'Gerdex', amount: 5, author_id: user.id, group_ids: ["", group.id]) }
@@ -18,7 +18,7 @@ RSpec.describe 'groups#index', type: :system do
   end
 
   scenario 'user can create a group' do
-    click_link('NEW CATEGORY')
+    click_link(id: 'create-category')
     expect(page).to have_content('New Category')
   end
 end
