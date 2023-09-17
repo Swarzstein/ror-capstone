ab6394a47b5cf25f974e656a5b08752df40fb877
Swarzstein <zorrillagja@gmail.com>
2023-09-17 01:08:27 -0400

    linters fixed for file added

--- spec/integration/group_show_spec.rb
+++ spec/integration/group_show_spec.rb
@@ -9,17 +9,17 @@ RSpec.describe 'GroupShow', type: :feature do
   end
   let!(:expense) { Expense.create(name: 'Gerdex', amount: 5, author_id: user.id, group_ids: ['', group.id]) }
   let!(:expense2) { Expense.create(name: 'Acetaminofen', amount: 7, author_id: user.id, group_ids: ['', group.id]) }
-  
+
   before :each do
     sign_in user
     visit group_path(group)
   end
-  
+
   it 'shows the group' do
     expect(page).to have_content('Gerdex')
     expect(page).to have_content('Acetaminofen')
   end
-  
+
   scenario 'user can create an expense' do
     click_link('NEW TRANSACTION')
     expect(page).to have_content('New Transaction')
