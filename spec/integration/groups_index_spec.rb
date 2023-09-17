1976ebca87802bd87957c41c72d1d383e6aa0250
Swarzstein <zorrillagja@gmail.com>
2023-09-17 00:31:40 -0400

    linters fixed

--- spec/integration/groups_index_spec.rb
+++ spec/integration/groups_index_spec.rb
@@ -4,7 +4,10 @@ require 'rails_helper'
 RSpec.describe 'groups#index', type: :feature do
   let!(:file) { File.open(Rails.root.join('app', 'assets', 'images', 'medicine.png')) }
   let!(:user) { User.create(name: 'Akai', email: 'akaiisred@gmail.com', password: '123123') }
-  let!(:group) { Group.create(name: 'Medicine', icon: { io: file, filename: 'medicine.png', content_type: 'image/png' }, user_id: user.id) }
+  let!(:group) do
+    Group.create(name: 'Medicine', icon: { io: file, filename: 'medicine.png', content_type: 'image/png' },
+                 user_id: user.id)
+  end
   let!(:expense) { Expense.create(name: 'Gerdex', amount: 5, author_id: user.id, group_ids: ['', group.id]) }
   let!(:expense2) { Expense.create(name: 'Acetaminofen', amount: 7, author_id: user.id, group_ids: ['', group.id]) }
 
